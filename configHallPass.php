d3c89b2b816d23dd5a87d51e7ea45639fc7e18f8
rmainhar <rmainhar@shakopee.k12.mn.us>
2017-11-07 12:10:26 -0600

    changed to have new versions work

--- configHallPass.php
+++ configHallPass.php
@@ -3,6 +3,6 @@
 		//mysql credentials
     $mysql_host = "localhost";
     $mysql_username = "root";
-    $mysql_password = "123456";
+    $mysql_password = "";
     $mysql_database = "hallpass";
 ?>
